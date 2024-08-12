22991079bbef33bbc6c9a4eab75242e84b2eb79e
Toscanisoft <toscanisoft@gmail.com>
2024-08-12 11:30:02 +0100

    installation of tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Fragment } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <Fragment>
+      <h1 className='text-3xl font-bold underline'>Hello, World!</h1>
+    </Fragment>
   )
 }
 
-export default App
+export default App;
